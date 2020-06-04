485d27250a9b71fe9c96c73df096f13675c6248e
emysmoura <emydl.moura@gmail.com>
2020-06-03 22:17:21 -0300

    finalizando back-end

--- server/src/database/migrations/00_create_points.ts
+++ server/src/database/migrations/00_create_points.ts
@@ -8,6 +8,7 @@ export async function up(knex: Knex){
         table.string('email').notNullable();
         table.string('whatsapp').notNullable();
         table.decimal('latitude').notNullable();              
+        table.decimal('longitude').notNullable();              
         table.string('city').notNullable();
         table.string('uf', 2).notNullable();
     })
