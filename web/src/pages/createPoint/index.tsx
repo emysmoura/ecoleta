5f6b330b4ac9605a5e3033772dce04ed53a8c1a7
emysmoura <emydl.moura@gmail.com>
2020-06-07 13:43:48 -0300

    finalizando aula 5 da nlw

--- web/src/pages/createPoint/index.tsx
+++ web/src/pages/createPoint/index.tsx
@@ -6,7 +6,8 @@ import { Map, TileLayer, Marker } from 'react-leaflet';
 import Api from '../../services/api';
 import axios from 'axios';
 import { LeafletMouseEvent } from 'leaflet';
-import './styles.css'
+import './styles.css';
+import Dropzone from '../../components/Dropzone';
 
 interface Item {
     id: number,
@@ -36,6 +37,7 @@ const CreatePoint = () => {
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+    const [selectedFile, setSelectedFile] = useState<File>();
     const history = useHistory();
 
     useEffect(() => {
@@ -113,15 +115,17 @@ const CreatePoint = () => {
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            name,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
+        const data = new FormData();
+        data.append('name', name);
+        data.append('email', email);
+        data.append('whatsapp', whatsapp);
+        data.append('uf', uf);
+        data.append('city', city);
+        data.append('latitude', String(latitude));
+        data.append('longitude', String(longitude));
+        data.append('items', items.join(','));
+        if (selectedFile) {
+            data.append('image', selectedFile);
         }
 
         Api.post('points', data).then(response => {
@@ -142,6 +146,7 @@ const CreatePoint = () => {
 
             <form onSubmit={HandleSubmit}>
                 <h1>Cadastro do<br /> ponto de coleta</h1>
+                <Dropzone onFileUploaded={setSelectedFile} />
                 <fieldset>
                     <legend>
                         <h2>Dados</h2>
